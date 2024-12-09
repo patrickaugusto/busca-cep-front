04688f8b8ebac1d59dcef6519b5ab1545eb01172
Patrick Augusto <trick0augusto@gmail.com>
2024-12-09 13:42:18 -0300

    update title

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,6 @@ import CepForm from './components/form/CepForm'
 import Footer from './components/footer/Footer'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <CepForm />
